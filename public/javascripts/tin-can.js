6b2b8ca939e433f247f0fd2f0e75f7fa406142db
hhd <henrydavidge@gmail.com>
2013-12-16 16:13:01 -0500

    Now works on Chrome

--- public/javascripts/tin-can.js
+++ public/javascripts/tin-can.js
@@ -1,8 +1,12 @@
-var cfg = {"iceServers":[{"url":"stun:23.21.150.121"}]}
-
+var cfg = {"iceServers":[{"url": "stun:stun.l.google.com:19302"}]};
 var peers = {};
-
 var name = "SnowedInRussia" + Math.floor(Math.random() * 1000);
+var mc = {
+	"mandatory": {
+	"OfferToReceiveAudio":true, 
+	"OfferToReceiveVideo":true
+	}
+};
 
 $("#cmd").html("hi");
 
@@ -31,8 +35,26 @@ function handleDC(e) {
 }
 
 function handleStream(e) {
+	console.log("Received media stream: ",  e);
 	var video = $('.video')[0];
 	attachMediaStream(video, e.stream);
+}
+
+function handleIceCandidate(e) {
+	console.log("ice candidate: ", e);
+	var c = e.target;
+	if (c.print && e.candidate) {
+		c.last_msg.ice.push(e.candidate);
+		$("#cmd").val(JSON.stringify(c.last_msg));
+	}
+	var msg = {
+		type: 'ice',
+		from: name,
+		to: c.remote,
+		last_hop: name,
+		data: e.candidate
+	}
+	send(msg, c.last_hop);
 }
 
 function createConnection(remote) {
@@ -40,14 +62,10 @@ function createConnection(remote) {
 
 	p.ondatachannel = handleDC;
 	p.onopen = function (e) {console.log("On open")};
-	p.onicecandidate = function (e) {
-		console.log("On ice candidate");
-		if (e.candidate) {
-			p.addIceCandidate(e.candidate);
-		}
-	}
+	p.onicecandidate = handleIceCandidate;
 	p.onaddstream = handleStream;
 	p.remote = remote;
+	p.print = false;
 
 	// Add to chat UI as well
 	App.addContact(remote);
@@ -56,14 +74,17 @@ function createConnection(remote) {
 
 function parseMessage(msg) {
 	msg = JSON.parse(msg.data);
-	console.log("Parsing a new message: " +  msg);
+	console.log("Parsing a new message: ", msg);
+
 	if (msg.to !== name) {
 		console.log("Forwarding msg to " + msg.to );
 		msg.last_hop = name;
 		peers[msg.to].dc.send(JSON.stringify(msg));
 		return;
 	}
-	switch (msg.type.valueOf()) {
+
+	//Update last hop
+	switch (msg.type) {
 		case "text":
 			handleChat(msg);
 			break;
@@ -76,51 +97,64 @@ function parseMessage(msg) {
 		case "req_intro":
 			handleIntro(msg);
 			break;
+		case "ice":
+			if (msg.from in peers) {
+				peers[msg.from].conn.addIceCandidate(new RTCIceCandidate(msg.data));
+			}
 	}
 }
 
 function handleIntro(msg, print) {
 	if (!(msg.from in peers)) {
-		// TODO: See if you need verification
+		// TODO: Get user verification
 		peers[msg.from] = {
 			'conn': createConnection(msg.from)
 		};
 	}
+	peers[msg.from].conn.last_hop = msg.last_hop;
+
 	var out_msg = {
 		'to': msg.from,
 		'from': name,
 		'type': 'offer',
 		'last_hop': name
 	};
-	var o;
+
+	// Create the datachannel
 	peers[msg.from].dc = peers[msg.from].conn.createDataChannel(msg.from, null);
+
+	// Attach the media stream if it exists
 	if ("local_stream" in window) {
+		console.log("Adding media stream");
+		local_stream.remote = name;
 		peers[msg.from].conn.addStream(local_stream);
 	}
+
 	peers[msg.from].conn.createOffer(function (offer) {
 		out_msg.data  = offer;
 		peers[msg.from].conn.setLocalDescription(offer);
-		console.log(JSON.stringify(out_msg));
+
+		// If we're still in the copy-and-paste stage
 		if (print) {
-			console.log("Should print");
+			peers[msg.from].conn.print = true;
+			out_msg.ice = [];
+			peers[msg.from].conn.last_msg = out_msg;
 			$("#cmd").val(JSON.stringify(out_msg));
 		}
 		send(out_msg, msg.last_hop);
-	}, function() {console.warn("fuuuuuu")});
-	console.log(o);
+	}, function() {console.warn("fuuuuuu")}, mc);
 }
 	
 
 function handleOffer(msg, print) {
 	if (!(msg.from in peers)) {
-		// TODO: See if you need verification
+		// TODO: Get user verification
 		peers[msg.from] = {
 			'conn': createConnection(msg.from),
 		};
 	}
-	var offer = new RTCSessionDescription(msg.data);
-	peers[msg.from].conn.setRemoteDescription(offer);
-		
+	peers[msg.from].conn.last_hop = msg.last_hop;
+
 	var out_msg = {
 		'to': msg.from,
 		'from': name,
@@ -129,37 +163,58 @@ function handleOffer(msg, print) {
 	};
 
 	if ("local_stream" in window) {
+		console.log("Adding media stream");
 		peers[msg.from].conn.addStream(local_stream);
 	}
+
+	var offer = new RTCSessionDescription(msg.data);
+	peers[msg.from].conn.setRemoteDescription(offer);
+
+	// Handle ice candidates if there are any
+	for (var i = 0; 'ice' in msg && i < msg.ice.length; i++) {
+		peers[msg.from].conn.addIceCandidate(new RTCIceCandidate(msg.ice[i]));
+	}
+
 	peers[msg.from].conn.createAnswer(function (answer) {
 		out_msg.data = answer;
 		peers[msg.from].conn.setLocalDescription(answer);
 		console.log(JSON.stringify(out_msg));
 		if (print) {
+			peers[msg.from].conn.print = true;
+			out_msg.ice = [];
+			peers[msg.from].conn.last_msg = out_msg;
 			$("#cmd").val(JSON.stringify(out_msg));
 		}
 		send(out_msg, msg.last_hop);
 	}, function() {
 		console.warn("Unable to create answer wtf");
-	});
+	}, mc);
 	return 
 }
 
 function handleAnswer(msg, print) {
 	if (!(msg.from in peers)) {
 		console.warn("Answer from unknown peer... weird");
-	} else {
-		try {
-			var answer = new RTCSessionDescription(msg.data);
-			peers[msg.from].conn.setRemoteDescription(answer);
-			if (print) {
-				$("#cmd").val("Commands can go here");
-			}
-			peers[msg.from].dc.onmessage = parseMessage;
-			console.log("DC to " + msg.from + " connected");
-		} catch (err) {
-			console.warn('Data channel is fucked', err);
+		return;
+	}
+
+	try {
+
+		var answer = new RTCSessionDescription(msg.data);
+		peers[msg.from].conn.setRemoteDescription(answer);
+		
+		// Handle ice candidates if there are any
+		for (var i = 0; 'ice' in msg && i < msg.ice.length; i++) {
+			peers[msg.from].conn.addIceCandidate(new RTCIceCandidate(msg.ice[i]));
 		}
+
+		if (print) {
+			$("#cmd").val("Commands can go here");
+		}
+		peers[msg.from].dc.onmessage = parseMessage;
+		console.log("DC to " + msg.from + " connected");
+	} catch (err) {
+		console.warn('Data channel is fucked', err);
 	}
 }
 
