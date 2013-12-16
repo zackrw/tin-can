8b9134d04874c9dd41fac03121e474be2c69806a
Zack Reneau-Wedeen <xaq2892@yahoo.com>
2013-12-15 17:39:01 -0800

    Update chat to work, and style it.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -59,36 +59,19 @@ $(function() {
 
   var friends = $('.friends');
   var chatContainer = $('.chat-container');
+  var chatWrite = $('.chat-write');
 
   // templates
   var friendTemplate = $('.friend.template');
   var chatTemplate = $('.chat.template');
 
   // TODO: CONVERT TO A FUNCTION WHICH CAN BE CALLED FOR INDIVIDUAL CHATS.
-  $('.friends').on('click', '.friend', function() {
+  friends.on('click', '.friend', function() {
     var conversationId = $(this).attr('data-id');
     if (conversationId !== chatContainer.attr('data-id')) {
-      var conversation = App.findConversation(conversationId);
-      chatContainer.find('.chats').empty();
-      chatContainer.find('.name').text(conversation.name);
-      chatContainer.attr('data-id', conversationId);
-      var chats = conversation.chats;
-      var i, text, time;
-      for (i = 0; i < chats.length; i++) {
-        text = chats[i].text;
-        time = chats[i].time;
-        var fromClass = chats[i].mine ? 'mine' : 'theirs';
-        chat = chatTemplate.clone().removeClass('template').
-                                    addClass(fromClass);
-
-        chat.find('.text').text(text);
-        chat.find('.time').text(time);
-        chatContainer.find('.chats').append(chat);
-      }
-
+      App.selectConversation(conversationId);
     }
   });
-
 
   App.populate = function() {
     var i, li, conversation;
@@ -110,7 +93,54 @@ $(function() {
     }
   };
 
+  App.makeChat = function(chat) {
+    var fromClass = chat.mine ? 'mine' : 'theirs';
+    var chatEl = chatTemplate.clone().removeClass('template').
+                                addClass(fromClass);
+    chatEl.find('.text').text(chat.text);
+    chatEl.find('.time').text(chat.time.toLocaleTimeString());
+    chatContainer.find('.chats').append(chatEl);
+  };
+
+  App.selectConversation = function(id) {
+    var conversation = App.findConversation(id);
+    chatContainer.find('.chats').empty();
+    chatContainer.find('.name').text(conversation.name);
+    chatContainer.attr('data-id', id);
+    var chats = conversation.chats;
+    var i, text, time;
+    for (i = 0; i < chats.length; i++) {
+      App.makeChat(chats[i]);
+    }
+    App.selectedConversation = id;
+    $('.chats').animate({ scrollTop: $('.chats').height() }, 300);
+  };
+
+  App.addChat = function(text) {
+    var conversation = App.findConversation(chatContainer.attr('data-id'));
+    var chat = {
+      text: text,
+      time: new Date,
+      mine: true
+    };
+    conversation.chats.push(chat);
+    App.makeChat(chat);
+    $('.chats').animate({ scrollTop: $('.chats').height() }, 300);
+  };
+
+
+  // populate conversations.
   App.populate();
+  // initialize to the first conversation.
+  App.selectConversation(App.conversations[0].id);
+
+  chatWrite.keypress(function(e) {
+    if (e.which === 13 && $(this).val() !== '') {
+      App.addChat($(this).val());
+      $(this).val('');
+    }
+  });
+
 
 });
 
